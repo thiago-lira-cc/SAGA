7232c9c843261dc9e160dd8e0a97eaedcc73d2b7
thiago-lira-cc <thiago.lira@ccc.ufcg.edu.br>
2019-10-02 15:38:12 -0300

    Novos testes

---
+++ testes/SAGA/testeProduto.java
@@ -0,0 +1,46 @@
+package SAGA;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class testeProduto {
+
+	private Produto produto;
+	private Produto produto2;
+	private Produto produto3;
+	
+	@BeforeEach
+	public void criaProduto() {
+		produto = new Produto("Suco", "Suco de laranja", "2.50");
+		produto2 = new Produto("Doce", "Doce de banana", "4.00");
+		produto3 = new Produto("Suco", "Suco de laranja");
+	}
+	
+	@Test
+	public void testToStringProduto() {
+		assertEquals("Suco - Suco de laranja - 2.50", produto.toString());
+	}
+	
+	@Test
+	public void testHasCodeProduto() {
+		assertEquals(true, produto.hashCode()==produto3.hashCode());
+		assertEquals(false, produto.hashCode()==produto2.hashCode());
+	}
+	
+	@Test
+	public void testEqualsProduto() {
+		assertEquals(true, produto.equals(produto3));
+		assertEquals(false, produto.equals(produto2));
+	}
+	
+	@Test
+	public void testSetPreco() {
+		assertEquals("Suco - Suco de laranja - 2.50", produto.toString());
+		produto.setPreco("1.50");
+		assertEquals("Suco - Suco de laranja - 1.50", produto.toString());
+	}
+
+
+}
